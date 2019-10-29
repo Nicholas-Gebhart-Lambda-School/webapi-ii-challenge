c36727ec09d8544b830e72a56a3b22b64b75b5dc
unknown <nicholas.gebhart@gmail.com>
2019-10-29 12:33:06 -0700

    feat: add post requests
    add new post
    add new comment

--- posts/posts.router.js
+++ posts/posts.router.js
@@ -11,6 +11,53 @@ const {
   insertComment
 } = db;
 
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else if (title && contents) {
+    insert({ title, contents })
+      .then(postid => {
+        const id = postid.id;
+        res.status(201).json({ title, contents, id });
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
+router.post("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  const { text, post_id } = req.body;
+  id &&
+    text &&
+    id === post_id &&
+    insertComment({ text, post_id })
+      .then(comment => {
+        res.status(201).json(comment);
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+
+  if (!id || id !== post_id) {
+    res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist." });
+  } else if (!text) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+  }
+});
+
 router.get("/", (req, res) => {
   find()
     .then(posts => res.status(200).json(posts))

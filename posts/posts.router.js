7e111e38325d614774cad02fd9fe9ba2aae7a41c
unknown <nicholas.gebhart@gmail.com>
2019-10-29 12:04:48 -0700

    feat: add comment by id
    get comments array by post id

--- posts/posts.router.js
+++ posts/posts.router.js
@@ -21,22 +21,31 @@ router.get("/", (req, res) => {
     );
 });
 
-// If the post with the specified id is not found:
-
-// return HTTP status code 404 (Not Found).
-// return the following JSON object: { message: "The post with the specified ID does not exist." }.
-// If there's an error in retrieving the post from the database:
-
-// cancel the request.
-// respond with HTTP status code 500.
-// return the following JSON object: { error: "The post information could not be retrieved." }.
-
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   findById(id)
     .then(post => {
       if (post.length) {
         res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  findPostComments(id)
+    .then(comments => {
+      if (comments.length) {
+        res.status(200).json(comments);
       } else {
         res
           .status(404)
